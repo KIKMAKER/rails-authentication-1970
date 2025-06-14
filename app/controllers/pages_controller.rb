a4226fe9e88ff126c9db720461eb498412913f47
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-06-14 10:46:41 +0200

    route to flatshow and something message in the controller rendered in flatshow.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  
+
   def home
+  end
+  def flatshow
+    @something = "A message from the db since we don't have a flats model yet"
   end
 end
